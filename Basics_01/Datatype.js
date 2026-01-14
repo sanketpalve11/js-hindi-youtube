abe5b02107ccd4817daee9d91afb9f57443e3193
sanketpalve11 <sanketpalve11@gmail.com>
2026-01-14 08:55:49 +0000

    Variables

---
+++ Basics_01/Datatype.js
@@ -0,0 +1,15 @@
+// Data types
+
+// number -range of number is 2 to the power 53
+// bigInt 
+//string ""
+//boolean -true/false 
+//null- standalone value
+//undefined =>
+// symbol - unique 
+
+
+// object
+
+console.log(typeof undefined )
+console.log(typeof null)
---
+++ Basics_01/Variables.js
@@ -0,0 +1,8 @@
+const acc_id = 23344
+let acc_email = "sakel@gmail.com"
+var acc_pass = "24344"
+acc_city ="jaipir"
+// acc_id = 59503   not allowed constant cannot be changed 
+console.log(acc_id)
+
+console.table([acc_id, acc_email, acc_pass, acc_city])
