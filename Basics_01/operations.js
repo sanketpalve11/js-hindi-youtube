303512b3df8180c2a10553e2ed0a11244a97fc06
sanketpalve11 <sanketpalve11@gmail.com>
2026-01-16 08:04:41 +0000

    operations

---
+++ Basics_01/operations.js
@@ -0,0 +1,26 @@
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+// Basic mathamateical operations
+//console.log(2+2);
+//console.log(2/2);
+//console.log(2-2);
+//console.log(2%2);
+//console.log(2*2);
+
+string1 = "hello"
+string2 = " watsupp"
+string3 = string1 + string2
+//console.log(string3);
+
+
+//console.log("1"+ 2);
+//console.log(1 +"2");
+//console.log("1" + 2 + 3);
+//console.log(1  + 2 + "3");
+
+
+
+
